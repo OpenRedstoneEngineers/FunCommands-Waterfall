b1bc6f3ab2324105348d8290e30e6b53ac8a62da
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2019-07-12 21:27:16 +0300

    Refactor some DynamicCommand things

--- src/main/java/org/openredstone/commands/Reload.java
+++ src/main/java/org/openredstone/commands/Reload.java
@@ -1,5 +1,6 @@
 package org.openredstone.commands;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
@@ -11,30 +12,30 @@ import org.openredstone.handlers.DynamicCommandHandler;
 import java.io.IOException;
 
 public class Reload extends Command {
-
     Plugin plugin;
 
     public Reload(Plugin plugin) {
         super("funreload");
         this.plugin = plugin;
     }
+
     @Override
     public void execute(CommandSender commandSender, String[] strings) {
-        if(!commandSender.hasPermission("funcommands" + this.getClass().getSimpleName())){
+        if (!commandSender.hasPermission("funcommands" + this.getClass().getSimpleName())) {
             commandSender.sendMessage(new TextComponent("You do not have permission to run this command!"));
             return;
         }
-        DynamicCommandHandler.unRegisterCommands(plugin);
 
         try {
+            DynamicCommandHandler.unregisterCommands(plugin);
             DynamicCommandHandler.readCommands();
-        } catch (IOException e) {
-            FunCommands.logger.severe("Unable to read commands something is not right");
-            return;
+            DynamicCommandHandler.registerCommands(plugin);
+            DerpHandler.loadDerps();
+        } catch (Exception e) {
+            // TODO: nail down the possible exceptions and report them in a better way
+            e.printStackTrace();
+            commandSender.sendMessage(new TextComponent("Could not reload FunCommands"));
         }
-
-        DynamicCommandHandler.registerCommands(plugin);
-        DerpHandler.loadDerps();
         commandSender.sendMessage(new TextComponent("Reloaded FunCommands."));
     }
 }
